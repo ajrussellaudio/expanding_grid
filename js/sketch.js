d5ce5eb4dd4b1eba08f92991cd5720816779e28b
Alan Russell <ajrussellaudio@gmail.com>
2017-07-25 13:44:28 +0100

    refactor to use vectors

--- js/sketch.js
+++ js/sketch.js
@@ -17,17 +17,17 @@ function drawShape(x, y, spaceBetween, lineLength) {
   fill("#333333")
   stroke(255);
   strokeWeight(5);
-  var c1 = getVertex(x, y, lineLength);
-  var c2 = getVertex(x + spaceBetween, y, lineLength);
-  var c3 = getVertex(x + spaceBetween, y + spaceBetween, lineLength);
-  var c4 = getVertex(x, y + spaceBetween, lineLength);
-  // console.log(c1);
+  var mousePosition = createVector(mouseX, mouseY);
+  var c1 = getVector(createVector(x, y), mousePosition, lineLength);
+  var c2 = getVector(createVector(x + spaceBetween, y), mousePosition, lineLength);
+  var c3 = getVector(createVector(x + spaceBetween, y + spaceBetween), mousePosition, lineLength);
+  var c4 = getVector(createVector(x, y + spaceBetween), mousePosition, lineLength);
   quad(c1.x, c1.y, c2.x, c2.y, c3.x, c3.y, c4.x, c4.y);
 }
 
-function getVertex(x, y, maxLength) {
-  var distance = dist(x, y, mouseX, mouseY);
+function getVector(center, position, maxLength) {
+  var distance = dist(center.x, center.y, position.x, position.y);
   var length = constrain(distance, 0, maxLength * 2);
-  var angle = atan2(y - mouseY, x - mouseX);
-  return createVector(x + cos(angle) * length, y + sin(angle) * length);
+  var angle = atan2(center.y - position.y, center.x - position.x);
+  return createVector(center.x + cos(angle) * length, center.y + sin(angle) * length);
 }
